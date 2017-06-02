0276b2d42be5ae3615285751aed63c4027fd53c4
boogoogle <1099594257@qq.com>
2017-06-03 00:30:40 +0800

    007

--- 007_dispatch_async_action.js
+++ 007_dispatch_async_action.js
@@ -1,14 +1,14 @@
 /**
  * 异步action
  *
- * 前面我们看到了怎样去发起一个行为(dispatch action) ,并且知道了这些actions通过对应的reducers,改变了应用的状态
+ * 前面我们看到了怎样去发起一个行为(dispatch action) ,并且知道了这些actions通过对应的reducers,怎样改变了应用的状态
  *
  * 但是到此为止,我们只是考虑了同步的action,确切的说,actionCreators是同步创建action的
  * 当我们发起一个动作(action),这些actionCreators 是立即返回对应数据的
  *
  * 现在考虑一种异步的场景,假设
  * 	1. 用户点击了一个按钮,按钮上写着"Say hi 2 seconds"
- * 	2. 点击按钮A,两秒后消息显示为hi
+ * 	2. 点击按钮A,两秒后,把数据hi保存到对应的字段中
  * 	3. 两秒后,更新view为Hi
  *
  * 很明显这条消息是我们应用状态(state)的一部分,所以我们必须把它存在Redux的store里
@@ -35,15 +35,25 @@ var reducer = combineReducers({
 })
 
 var store_0 = createStore(reducer)
-var sayActionCreator = function(message) {
-	// 注意,直接这样写是不生效的,你会发现,reducer接收到的action是一个undefined
-	// setTimeout(function(){ 
-	// 	return {
-	// 		type: "SAY",
-	// 		message
-	// 	}
-	// }, 2000)
 
+// 注意,直接这样写是不生效的,你会发现,reducer接收到的action是一个undefined
+var asyncSayActionCreator_0 = function (message) {
+    setTimeout(function () {
+        return {
+            type: 'SAY',
+            message
+        }
+    }, 2000)
+}
+store_0.dispatch(sayActionCreator_1('Hi'))
+// 想一下这里的actionCreator是否会生效?
+// 我们说过actionCreator的返回值应该是什么?
+// setTimeout这个表达式返回值是什么呢?
+
+
+// 请务必解答上面三个问题,再看下面代码
+var sayActionCreator_1 = function(message) {
+	
 	// 这里有一个技巧,除了直接返回一个action,我们的actionReducer还可以返回一个函数,
 	// 这个函数在之后可以dispatch一个action
 	// 但是如果我们想要让这个函数拥有dispatch action的能力,我们需要传入dispatch这个参数
@@ -55,12 +65,12 @@ var sayActionCreator = function(message) {
 			})
 		}, 2000)
 	}
-	// 仅仅这么写的话, ActionCreator返回了一个方法,这样redux是会报错的,因为action只能
+	// 仅仅这么写的话, ActionCreator返回了一个方法,这样redux是会报错的,因为Redux能够接受的action只能
 	// 是纯对象,所以这类需要用到一些middleware(中间件)
 	
 }
 
-store_0.dispatch(sayActionCreator('Hi'))
+store_0.dispatch(sayActionCreator_1('Hi'))
 
 
 // 中间件请看-> 008_middleware.js
